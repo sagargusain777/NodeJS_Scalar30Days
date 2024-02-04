f3ad416ce503a28b3bf71915d91b7e1c43fcbe48
uwuxverticuse <sagargusain150@gmail.com>
2024-02-04 16:39:31 +0530

    First Node Program

---
+++ firstProgram.js
@@ -0,0 +1,8 @@
+console.log("I am Happy to learn Node JS")
+function sayHello(){
+    console.log("Hey Node JS I am Happy while Learning you !!!")
+
+}
+sayHello();
+let name = "Sagar";
+console.log(global.name);
