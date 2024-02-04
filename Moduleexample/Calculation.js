7c6b18593b9fa4e5e6faf1c0fa95bc98f2c7d5b6
uwuxverticuse <sagargusain150@gmail.com>
2024-02-04 16:41:52 +0530

    Module Example

---
+++ Moduleexample/Calculation.js
@@ -0,0 +1,30 @@
+
+//Formula for addition
+function add(a,b){
+
+    console.log(a+b);
+
+}
+//Formula for subtraction
+function sub(a,b){
+    console.log(a-b);
+
+}
+//Formula for multiplication
+function multpy(a,b){
+    console.log(a*b);
+
+}
+//Formula for division
+function div(a,b){
+    console.log(a/b);
+
+}
+
+module.exports={
+    addition: add,
+    subtraction: sub,
+    multiplication: multpy,
+    division: div,
+    
+}
---
+++ Moduleexample/Calculator.js
@@ -0,0 +1,13 @@
+//Importing the module Calulation
+const calculator = require('./Calculation.js');
+
+
+//Addition of Two Numbers
+calculator.addition(3,4);
+//Subtraction of Two Numbers 
+calculator.subtraction(7,9);
+//Multiplication of Two Numbers
+calculator.multiplication(9,6);
+//Division of Two Numbers
+calculator.division(20,5);
+
