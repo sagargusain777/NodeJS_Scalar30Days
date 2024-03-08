b467b2c0c5415564346a98a840c696113c413dd3
uwuxverticuse <sagargusain150@gmail.com>
2024-03-08 12:50:58 +0530

    Completed Frontend and backend

--- ChaiAurCodeBackend/Project1/frontend/src/App.jsx
+++ ChaiAurCodeBackend/Project1/frontend/src/App.jsx
@@ -1,27 +1,33 @@
-
-import './App.css'
-import {useState} from 'react'
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [jokes, setJokes] = useState([]);
 
-  const [jokes,setJokes]= useState([]);
-  
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
 
   return (
     <>
       <h1> Connecting a Frontend and Backend</h1>
       <h2> Jokes {jokes.length}</h2>
-      {
-        jokes.map((joke,index)=>(
-          <div key={index}>
-            <h3>joke.id</h3>
-            <h4>joke.title</h4>
-            <p>joke.content</p>
-          </div>
-        ))
-      }
-      
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.id}</h3>
+          <h4>{joke.title}</h4>
+          <p>{joke.content}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
