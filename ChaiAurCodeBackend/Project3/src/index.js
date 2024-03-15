5fab96c21b7388f5ce266648323ec9377c5167d7
uwuxverticuse <sagargusain150@gmail.com>
2024-03-15 17:33:35 +0530

    Config : Configured dot env and Database

---
+++ ChaiAurCodeBackend/Project3/src/index.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+import  connectDB from './db/index.js';
+import express from 'express';
+
+dotenv.config( {path : './.env'})
+
+connectDB();
+
+
+
+
+
+/*import mongoose from "mongoose";
+import { DB_NAME } from "./constant";
+import express from 'express';
+
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error",(error) => {
+        console.error("Error: ", error)
+    
+    throw error;}
+    );
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    
+    });
+
+
+  } catch (error) {
+    console.error("Error :", error);
+  }
+})();*/
